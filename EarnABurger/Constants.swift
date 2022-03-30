d6b409c080bdab9d73622fe0a99d478700fc5eed
Zahra Kermani <zahrapkermani@gmail.com>
2022-03-30 12:28:05 +0200

    Added picker view delegate methods

---
+++ EarnABurger/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  EarnABurger
+//
+//  Created by Zahra Sadeghipoor on 3/30/22.
+//
+
+import Foundation
+
+
+let activities = ["Running", "Walking"]
+let statsFrequencies = ["2 min", "5 min", "10 min", "20 min", "30 min"]
+
--- EarnABurger/ViewController.swift
+++ EarnABurger/ViewController.swift
@@ -11,15 +11,58 @@ import UIKit
 // <a href="https://www.flaticon.com/free-icons/run" title="run icons">Run icons created by Freepik - Flaticon</a>
 // <a href="https://www.flaticon.com/free-icons/burger" title="burger icons">Burger icons created by Freepik - Flaticon</a>
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
 
+    @IBOutlet weak var StatFrequencyPickerView: UIPickerView!
+    @IBOutlet weak var ActivityPickerView: UIPickerView!
     @IBOutlet weak var ProfileImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         ProfileImageView.layer.cornerRadius = 0.1 * ProfileImageView.frame.height
+        
+        assignPickerViewDelegate()
+        
     }
+    
+    func assignPickerViewDelegate() {
+        ActivityPickerView.dataSource = self
+        ActivityPickerView.delegate = self
 
-
+        StatFrequencyPickerView.dataSource = self
+        StatFrequencyPickerView.delegate = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == ActivityPickerView {
+            return activities.count
+        }
+        if pickerView == StatFrequencyPickerView {
+            return statsFrequencies.count
+        }
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        
+        var title: String?
+        
+        if pickerView == ActivityPickerView {
+            title = activities[row]
+        }
+        if pickerView == StatFrequencyPickerView {
+            title = statsFrequencies[row]
+        }
+        
+        if let safeTitle = title {
+            return NSAttributedString(string: safeTitle, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        }
+        return nil
+    }
+    
 }
-
